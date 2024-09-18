bce4a612c7a882dc83ba4c725ab70a988dd1a4cd
Gabriel Fortes Ashikaga <gabriel.ashikaga@fatec.sp.gov.br>
2024-09-18 19:42:11 -0300

    :tada: Início da aula do dia 18-09-2024

--- App.js
+++ App.js
@@ -4,8 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Texto 1</Text>
+      <Text>Texto 2</Text>
+      <Text>Texto 3</Text>
+      <Text>Texto 4</Text>
     </View>
   );
 }
